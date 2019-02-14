375c218e7373d61dd3e6531edfe7080c964aecbc
Alex Brzosko <abrzosko9056@jh-mba158-01.local>
2019-02-14 11:11:30 -0600

    count added

---
+++ Item.swift
@@ -0,0 +1,20 @@
+//
+//  Item.swift
+//  shoppingList
+//
+//  Created by Alex Brzosko on 2/12/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var name: String
+    var count: Int
+    init(name:String, count:Int) {
+        self.name = name
+        self.count = count
+    }
+    
+    
+}
+
--- shoppingList/ViewController.swift
+++ shoppingList/ViewController.swift
@@ -1,20 +1,64 @@
 //
 //  ViewController.swift
-//  shoppingList
+// shoppingList
 //
-//  Created by Alex Brzosko on 2/8/19.
+//  Created by Alex Brzosko on 02/08/1994.
 //  Copyright © 2019 John Hersey High School. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
+ class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var newItemTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items: [Item] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        
+        let item1 = Item(name: "Milk", count: 2)
+        let item2 = Item(name: "Eggs", count: 3)
+        let item3 = Item(name: "Bread", count: 4)
+        items = [item1, item2, item3]
+        
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIBarButtonItem) {
+        if let newitemName = newItemTextField.text {
+            let newItem = Item(name: newitemName, count: 1)
+            items.append(newItem)
+            tableView.reloadData()
+        }
+        
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "myCell") {
+        let itemName = items[indexPath.row].name
+        cell.textLabel?.text = itemName
+        let countofItem = items[indexPath.row].count
+        
+        cell.detailTextLabel?.text = "Quantity: \(countofItem)"
+        
+        return cell
+        } else {
+            
+            return UITableViewCell()
+            
+        }
+    
+    
+    
+    
 }
 
+}
